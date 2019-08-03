b452078519bacfcf4866cf7c1a4cf883017f09a8
Eric Gouyer <folays@MBP-Folays-Online.local>
2019-08-03 06:09:18 +0200

    add support for sticky bit on directories

--- conversions.go
+++ conversions.go
@@ -868,6 +868,12 @@ func convertAttributes(
 	if in.Mode&os.ModeSetuid != 0 {
 		out.Mode |= syscall.S_ISUID
 	}
+	if in.Mode&os.ModeSetgid != 0 {
+		out.Mode |= syscall.S_ISGID
+	}
+	if in.Mode&os.ModeSticky != 0 {
+		out.Mode |= syscall.S_ISVTX
+	}
 }
 
 // Convert an absolute cache expiration time to a relative time from now for
@@ -923,6 +929,9 @@ func convertFileMode(unixMode uint32) os.FileMode {
 	}
 	if unixMode&syscall.S_ISGID != 0 {
 		mode |= os.ModeSetgid
+	}
+	if unixMode&syscall.S_ISVTX != 0 {
+		mode |= os.ModeSticky
 	}
 	return mode
 }
